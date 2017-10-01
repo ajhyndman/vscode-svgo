5c0d43c88de042abdc01fbecd8be2415c8f9e045
Andrew Hyndman <ajhyndman@hotmail.com>
2017-10-01 17:14:24 +1100

    Add optimizeSelection command

--- src/extension.ts
+++ src/extension.ts
@@ -17,7 +17,7 @@ export function activate(context: vscode.ExtensionContext) {
   }
 
   // Register VS Code commands.
-  const disposable = vscode.commands.registerCommand(
+  const disposable1 = vscode.commands.registerCommand(
     'extension.optimizeActiveFile',
     () => {
       if (svgo == null) {
@@ -29,12 +29,10 @@ export function activate(context: vscode.ExtensionContext) {
 
       // Get active editor contents.
       const activeEditorContents = vscode.window.activeTextEditor.document.getText();
-      console.log('current file contents', activeEditorContents);
 
       try {
         svgo.optimize(activeEditorContents, ({ data }) => {
           // Replace entire active editor contents
-          console.log('optimisation result', data);
           replaceSelection(
             vscode.window.activeTextEditor,
             getFullDocumentRange(vscode.window.activeTextEditor.document),
@@ -42,13 +40,41 @@ export function activate(context: vscode.ExtensionContext) {
           );
         });
       } catch (e) {
-        console.log(e);
         vscode.window.showErrorMessage(e.toString());
       }
     },
   );
 
-  context.subscriptions.push(disposable);
+  const disposable2 = vscode.commands.registerCommand(
+    'extension.optimizeSelection',
+    () => {
+      if (svgo == null) {
+        vscode.window.showErrorMessage(
+          'The svgo package is not available. Please install it locally or globally, and reload VS Code.',
+        );
+        return;
+      }
+
+      // Iterate over all active selections.
+      vscode.window.activeTextEditor.selections.forEach(selection => {
+        // Get text in current selection
+        const selectedText = vscode.window.activeTextEditor.document.getText(
+          selection,
+        );
+
+        try {
+          svgo.optimize(selectedText, ({ data }) => {
+            // Replace selection contents
+            replaceSelection(vscode.window.activeTextEditor, selection, data);
+          });
+        } catch (e) {
+          vscode.window.showErrorMessage(e.toString());
+        }
+      });
+    },
+  );
+
+  context.subscriptions.push(disposable1, disposable2);
 }
 
 export function deactivate() {}
